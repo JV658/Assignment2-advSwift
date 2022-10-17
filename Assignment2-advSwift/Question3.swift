431a31ec16200af625f23758097ad250cf2f665e
Joshua Vand der Most <joshua.vandermost@cambriancollege.ca>
2022-10-17 19:49:25 -0400

    fixed question 3 inits

--- Assignment2-advSwift/Question3.swift
+++ Assignment2-advSwift/Question3.swift
@@ -19,7 +19,7 @@ import Foundation
  - state: String
  - temp: Double
  methods:
- - init(cpu: String, gpu: String, ram: Int)
+ - init(cpu: String, gpu: String, ram: Int, state: String, temp: Double)
  - upgradeRam(newAmount: Int)
  - turnOn() => will set the state to 'On'
  - turnOff() => will set the state to 'Off'
@@ -34,7 +34,7 @@ import Foundation
  - screenSize: Double
  - isTouchScreen: Bool
  methods:
- - init(cpu: String, gpu: String, ram: Int, isRamUpgradable: Bool, screenSize: Double, isTouchScreen: Bool)
+ - init(cpu: String, gpu: String, ram: Int, state: String, temp: Double, isRamUpgradable: Bool, screenSize: Double, isTouchScreen: Bool)
  - override upgradeRam() => if the ram is not upgradable print to the console "RAM cannot be upgraded for this laptop"
  - closeScreen() => this will set the laptop state to 'Sleep'
  */
